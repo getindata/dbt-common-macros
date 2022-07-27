eb58f5772d0912af69d9177ecb1b40c051c769b8
piotrpekala <piotr.pekala@getindata.com>
2022-07-27 12:16:43 +0200

    Common macro

--- macros/custom_generate_schema_name.sql
+++ macros/custom_generate_schema_name.sql
@@ -1,4 +1,4 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
+{% macro custom_generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
     {%- if target.name == 'local' -%}
         {{ default_schema }}
